c870f87646498c14b7cf6c03ec5fc7288722da93
Mazhar-4321 <syedmazharali742@gmail.com>
2023-05-31 11:21:03 +0530

    Accordion

--- redux-sample/src/components/SampleAccordion.jsx
+++ redux-sample/src/components/SampleAccordion.jsx
@@ -1,44 +1,51 @@
-import * as React from 'react';
 import Accordion from '@mui/material/Accordion';
-import AccordionSummary from '@mui/material/AccordionSummary';
+import * as React from 'react'
 import AccordionDetails from '@mui/material/AccordionDetails';
+import AccordionSummary from '@mui/material/AccordionSummary';
 import Typography from '@mui/material/Typography';
 import ExpandMoreIcon from '@mui/icons-material/ExpandMore';
+import AddIcon from '@mui/icons-material/Add';
+import RemoveIcon from '@mui/icons-material/Remove';
 
 export default function SampleAccordion() {
+  const [expanded, setExpanded] = React.useState(false);
+  const [elements, setElements] = React.useState([false, false, false, false])
+  const handleChange = (panel) => (event, isExpanded) => {
+    console.log("kkkk",panel,isExpanded)
+    setExpanded(isExpanded ? panel : false);
+    let newElements = [...elements]
+    newElements[panel] = !newElements[panel]
+    setElements(newElements)
+  };
+
   return (
-    <div>
-      <Accordion>
-        <AccordionSummary
-          expandIcon={<ExpandMoreIcon />}
-          aria-controls="panel1a-content"
-          id="panel1a-header"
-        >
-          <Typography>Accordion 1</Typography>
-        </AccordionSummary>
-        <AccordionDetails>
-          <Typography>
-            Lorem ipsum dolor sit amet, consectetur adipiscing elit. Suspendisse
-            malesuada lacus ex, sit amet blandit leo lobortis eget.
-          </Typography>
-        </AccordionDetails>
-      </Accordion>
-      <Accordion>
-        <AccordionSummary
-          expandIcon={<ExpandMoreIcon />}
-          aria-controls="panel2a-content"
-          id="panel2a-header"
-        >
-          <Typography>Accordion 2</Typography>
-        </AccordionSummary>
-        <AccordionDetails>
-          <Typography>
-            Lorem ipsum dolor sit amet, consectetur adipiscing elit. Suspendisse
-            malesuada lacus ex, sit amet blandit leo lobortis eget.
-          </Typography>
-        </AccordionDetails>
-      </Accordion>
-    
+    <div style={{ display: 'flex', flexDirection: 'column', rowGap: '10px' }}>
+      {
+        elements.map((element, i) => {
+          return (
+            <Accordion expanded={elements[i]} onChange={handleChange(i)}>
+              <AccordionSummary
+                expandIcon={elements[i] ? <RemoveIcon /> : <AddIcon />}
+                aria-controls="panel1bh-content"
+                id="panel1bh-header"
+              >
+                <Typography sx={{ width: '33%', flexShrink: 0 }}>
+                  General settings
+                </Typography>
+                <Typography sx={{ color: 'text.secondary' }}>I am an accordion</Typography>
+              </AccordionSummary>
+              <AccordionDetails>
+                <Typography>
+                  Nulla facilisi. Phasellus sollicitudin nulla et quam mattis feugiat.
+                  Aliquam eget maximus est, id dignissim quam.
+                </Typography>
+              </AccordionDetails>
+            </Accordion>
+          )
+        })
+      }
+     
+
     </div>
   );
 }
